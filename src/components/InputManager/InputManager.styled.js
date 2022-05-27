78d7c08b7d7a0f860495b077541e0c17f36d222e
Steerpike <imthekoul@gmail.com>
2022-05-27 11:48:17 +0300

    Placed TaskManager on Grid

--- src/components/InputManager/InputManager.styled.js
+++ src/components/InputManager/InputManager.styled.js
@@ -4,6 +4,8 @@ const StyledInputManager = styled.div`
   padding: 20px;
   background-color: #fff;
   border: 2px solid #ddd;
+  grid-area: inputManager;
+  display: flex;
 `;
 
 export default StyledInputManager;
